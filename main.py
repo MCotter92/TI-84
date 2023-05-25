d3d14f9a94df7b21601d007e036e390f1af25099
maccotter11 <122413834+maccotter11@users.noreply.github.com>
2023-05-25 08:05:06 -0400

    push

---
+++ main.py
@@ -0,0 +1,37 @@
+def add(x, y):
+    """
+    Adds two objects together.
+    """
+    return x + y
+
+
+def subtract(x, y):
+    """
+    Subtracts one object from another.
+    """
+    return x - y
+
+
+def multiply(x, y):
+    """
+    Multiplies two objects together.
+    """
+    return x * y
+
+
+def divide(x, y):
+    """
+    Divides x by y.
+    """
+    return x / y
+
+
+def power(x, y):
+    """
+    Raises x to the power of y.
+    """
+    return x**y
+
+
+if __name__ == "__main__":
+    print(power(15, 5))
